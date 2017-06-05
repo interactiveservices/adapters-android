a7f8981a58c76af8f22d497de2a2a7c4a204df77
interactiveservices <beinteractive2011@gmail.com>
2017-06-05 15:00:45 +0300

    added file template for heterogeneous adapter (with two types of items)

--- adapters-android/src/main/java/su/ias/components/adapters/BaseListAdapter.java
+++ adapters-android/src/main/java/su/ias/components/adapters/BaseListAdapter.java
@@ -9,33 +9,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Base adapter for recyclerViews which contains a list of DateType
+ * Base adapter for recyclerViews which contains a list of DataType
  *
- * @param <DateType> type of class in List
+ * @param <DataType> type of class in List
  * @param <VH>       ViewHolder's type
  */
-public abstract class BaseListAdapter<DateType, VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
+public abstract class BaseListAdapter<DataType, VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
 
     protected final OnItemClickListener listener;
-    private final List<DateType> list;
+    private final List<DataType> list;
 
     public BaseListAdapter() {
         this.list = new ArrayList<>();
         this.listener = null;
     }
 
-    public BaseListAdapter(@Nullable List<DateType> list) {
-        this.list = (list == null ? new ArrayList<DateType>() : list);
+    public BaseListAdapter(@Nullable List<DataType> list) {
+        this.list = (list == null ? new ArrayList<DataType>() : list);
         this.listener = null;
     }
 
-    public BaseListAdapter(@Nullable List<DateType> list, @Nullable OnItemClickListener listener) {
-        this.list = (list == null ? new ArrayList<DateType>() : list);
+    public BaseListAdapter(@Nullable List<DataType> list, @Nullable OnItemClickListener listener) {
+        this.list = (list == null ? new ArrayList<DataType>() : list);
         this.listener = listener;
     }
 
-    public void add(DateType dateType) {
-        list.add(dateType);
+    public void add(DataType dataType) {
+        list.add(dataType);
         notifyItemInserted(list.size());
     }
 
@@ -44,16 +44,16 @@ public abstract class BaseListAdapter<DateType, VH extends RecyclerView.ViewHold
         notifyItemRemoved(position);
     }
 
-    public void remove(DateType item) {
+    public void remove(DataType item) {
         remove(list.indexOf(item));
     }
 
-    public void addAll(List<DateType> list) {
+    public void addAll(List<DataType> list) {
         this.list.addAll(list);
         notifyDataSetChanged();
     }
 
-    public DateType getItem(int position) {
+    public DataType getItem(int position) {
         return list.get(position);
     }
 
@@ -66,7 +66,7 @@ public abstract class BaseListAdapter<DateType, VH extends RecyclerView.ViewHold
         return LayoutInflater.from(parent.getContext());
     }
 
-    public List<DateType> getItems() {
+    public List<DataType> getItems() {
         return list;
     }
 }
